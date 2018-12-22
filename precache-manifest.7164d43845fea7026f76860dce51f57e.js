f33821a5fe7bb59edd5fb404f17aab78e6b44657
BPDesignsPNB <www.pablobeccalli@gmail.com>
2018-12-21 23:28:17 -0300

    Updates

---
+++ precache-manifest.7164d43845fea7026f76860dce51f57e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8337bf4d23bc4ce8c799e337bb07fb5",
+    "url": "/ReactContactManager/static/media/pattern.e8337bf4.png"
+  },
+  {
+    "revision": "b23854e90bfa8099d41649ddaff28dde",
+    "url": "/ReactContactManager/static/media/404d.b23854e9.png"
+  },
+  {
+    "revision": "0eada4cf4dcc9cac7673",
+    "url": "/ReactContactManager/static/js/runtime~main.0eada4cf.js"
+  },
+  {
+    "revision": "309a15846f82e78f4f94",
+    "url": "/ReactContactManager/static/js/main.309a1584.chunk.js"
+  },
+  {
+    "revision": "8dc8d1c2e5477d2c5035",
+    "url": "/ReactContactManager/static/js/1.8dc8d1c2.chunk.js"
+  },
+  {
+    "revision": "309a15846f82e78f4f94",
+    "url": "/ReactContactManager/static/css/main.3d650f7f.chunk.css"
+  },
+  {
+    "revision": "8dc8d1c2e5477d2c5035",
+    "url": "/ReactContactManager/static/css/1.b4b674f6.chunk.css"
+  },
+  {
+    "revision": "dc9e570eea75b1e7c008ceccf608de65",
+    "url": "/ReactContactManager/index.html"
+  }
+];
